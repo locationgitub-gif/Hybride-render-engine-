be6b1402472c305e50ce69aec8cedbfded0c3958
locationgitub-gif <locationgitub@gmail.com>
2026-04-14 22:32:35 +0200

    Refactor package and improve initialization logging
    Updated package structure and modified initialization logging.

--- HybridCore/fabric/src/main/java/com/denotas/hybridcore/fabric/HybridCoreFabric.java
+++ HybridCore/fabric/src/main/java/com/denotas/hybridcore/fabric/HybridCoreFabric.java
@@ -1,18 +1,12 @@
-package com.denotas.hybridcore.fabric;
+package com.denotas.hybridcore;
 
-import com.denotas.hybridcore.HybridCore;
-import com.denotas.hybridcore.gui.HybridHUD;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class HybridCoreFabric implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HybridCore mod = new HybridCore();
-        mod.init();
-
-        // Registar HUD
-        HudRenderCallback.EVENT.register(new HybridHUD(mod));
+        new HybridCore();
+        HybridCore.LOGGER.info("HybridCore iniciado com sucesso");
     }
 }
